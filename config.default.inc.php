4716e7470f984a651316ebc7669301ec74f2e745
onassar <onassar@gmail.com>
2020-07-27 12:17:09 -0400

    Default config file cleanup

--- config.default.inc.php
+++ config.default.inc.php
@@ -7,36 +7,20 @@
     namespace Plugin\MemcachedSession;
 
     /**
-     * Data
+     * Plugin Config Data
      * 
      */
-
-    // When does it expire
     $expiry = 0;
-
-    // Cookie/session naming
-    $name = 'session';
-
-    // Storage
-    $servers = array(
-        array('127.0.0.1', '11211')
-    );
-
-    // Host for cookie
     $host = '.' . ($_SERVER['HTTP_HOST']);
+    $name = 'session';
+    $servers = array();
+    $server = array('127.0.0.1', '11211');
+    array_push($servers, $server);
+    $pluginConfigData = compact('expiry', 'host', 'name', 'servers');
 
     /**
-     * Config storage
+     * Storage
      * 
      */
-
-    // Store
-    \Plugin\Config::add(
-        'TurtlePHP-MemcachedSessionPlugin',
-        array(
-            'expiry' => $expiry,
-            'name' => $name,
-            'servers' => $servers,
-            'host' => $host
-        )
-    );
+    $key = 'TurtlePHP-MemcachedSessionPlugin';
+    \Plugin\Config::add($key, $pluginConfigData);
